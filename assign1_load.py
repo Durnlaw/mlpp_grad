935120b5628d41b58011541c5632abfb93423495
Durnlaw <durnlaw@hotmail.com>
2021-09-07 20:53:04 -0400

    Small change to comments

--- assign1_load.py
+++ assign1_load.py
@@ -15,24 +15,24 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 #> Save our table structure
-# create_table = ("""
-#     CREATE TABLE ewinter_acs_data (
-#         index SERIAL PRIMARY KEY,
-#         name VARCHAR NOT NULL,
-#         pop_reporting_household VARCHAR NOT NULL,
-#         married_house VARCHAR NOT NULL,
-#         married_house_kids_under_18 VARCHAR NOT NULL,
-#         married_house_nokids_under_18 VARCHAR NOT NULL,
-#         female_no_spouse VARCHAR NOT NULL,
-#         male_no_spouse VARCHAR NOT NULL,
-#         state VARCHAR NOT NULL,
-#         county VARCHAR NOT NULL,
-#         tract VARCHAR NOT NULL,
-#         block_group VARCHAR NOT NULL
-#     )
-# """)
-# #. Make the table
-# cur.execute(create_table)
+create_table = ("""
+    CREATE TABLE ewinter_acs_data (
+        index SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        pop_reporting_household VARCHAR NOT NULL,
+        married_house VARCHAR NOT NULL,
+        married_house_kids_under_18 VARCHAR NOT NULL,
+        married_house_nokids_under_18 VARCHAR NOT NULL,
+        female_no_spouse VARCHAR NOT NULL,
+        male_no_spouse VARCHAR NOT NULL,
+        state VARCHAR NOT NULL,
+        county VARCHAR NOT NULL,
+        tract VARCHAR NOT NULL,
+        block_group VARCHAR NOT NULL
+    )
+""")
+#. Make the table
+cur.execute(create_table)
 
 #> Bring in the csv
 #. Referenced: https://www.dataquest.io/blog/loading-data-into-postgres/
